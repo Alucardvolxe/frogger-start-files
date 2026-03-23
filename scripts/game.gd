974366466bab1a3a96a797d3396b87bf935d83ef
Nortey Daniel Yeboah <gogamerdan247@gmail.com>
2026-03-23 23:30:58 +0000

    spawn in the car

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
+var car_scene: PackedScene = preload("res://scenes/Car.tscn")
 
 
 
 func _on_detect_body_entered(body: Node2D) -> void:
 	print(body)
 	print("Has entered")
+
+func _on_car_timer_timeout() -> void:
+	var car = car_scene.instantiate()
+	add_child(car)
