799777f9a0f9839e6d3c3c41be1585bc955777b0
Nortey Daniel Yeboah <gogamerdan247@gmail.com>
2026-03-25 15:05:26 +0000

    environment

--- scripts/car.gd
+++ scripts/car.gd
@@ -1,12 +1,15 @@
 extends Area2D
 
 var direction: Vector2= Vector2.LEFT
-
+var colors = [preload("res://graphics/cars/green.png"),
+				preload("res://graphics/cars/red.png"),
+				preload("res://graphics/cars/yellow.png")]
 var speed = 100
 func _ready() -> void:
 	if(position.x<0) :
 		direction.x=1
 		$Sprite2D.flip_h=true
+	$Sprite2D.texture= colors.pick_random()
 
 
 func _process(delta: float) -> void:
